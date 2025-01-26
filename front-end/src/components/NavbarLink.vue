518686c6b93909c4e5bc7071c97463dca06480a2
Kid A <thisismeamir@outlook.com>
2025-01-26 19:44:43 +0330

    [init] Back-end Module

--- front-end/src/components/NavbarLink.vue
+++ front-end/src/components/NavbarLink.vue
@@ -1,7 +1,8 @@
 
 <template>
 <router-link :to="route">
-     <div class=" nuke-text-heading-03  px-16px py-8px hover:px-32px   text-white hover:text-black hover:bg-white
+     <div class="
+     nuke-text-heading-03  px-16px py-8px hover:px-32px   text-white hover:text-black hover:bg-white
      nuke-transition-all nuke-transition-productive-standard-moderate-01 h-fit select-none text-nowrap xs:max-w-fit
 ">
           {{ title}}

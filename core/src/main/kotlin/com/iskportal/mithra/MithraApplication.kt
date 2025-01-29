6001fe319ae4703b71f9cd502e7ab969ce4e99c1
Kid A <thisismeamir@outlook.com>
2025-01-29 20:11:46 +0330

    [implementation]Switching to Spring

---
+++ core/src/main/kotlin/com/iskportal/mithra/MithraApplication.kt
@@ -0,0 +1,11 @@
+package com.iskportal.mithra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MithraApplication
+
+fun main(args: Array<String>) {
+	runApplication<MithraApplication>(*args)
+}

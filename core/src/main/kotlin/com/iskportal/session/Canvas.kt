24a07a5c2f91c4277223be1eff1b6a3889c61c28
Kid A <thisismeamir@outlook.com>
2025-01-29 11:20:24 +0330

    [implementation] Defining Canvas

---
+++ core/src/main/kotlin/com/iskportal/session/Canvas.kt
@@ -0,0 +1,10 @@
+package com.iskportal.session
+
+data class Canvas(
+    val content: CanvasContent,
+    val author: String,
+    val createdDate: String,
+    val lastChange: String,
+    val title: String
+)
+
